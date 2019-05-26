33a8613acbc7b46510188b5ef4cd658ed80d0788
Zev Stravitz <zevstravitz2021@u.northwestern.edu>
2019-05-26 00:24:31 -0500

    Works

--- nbody.py
+++ nbody.py
@@ -4,15 +4,15 @@ import math
 import numpy as np
 
 #CONSTANTS
-TIMESTEP = 1/100
+TIMESTEP = 0.00
 FRAMERATE = 1
-BODIES = 4
+BODIES = 3
 HEIGHT = 800
 WIDTH = 1200
 SIZE = 3
-VEL = 10
-MASS = 5
-G_CONSTANT = 3
+VEL = 1
+MASS = 2
+G_CONSTANT = 100
 
 class Particle:
     def __init__(self):
@@ -25,36 +25,38 @@ class Particle:
         self.mass = np.random.uniform(MASS, MASS * 5, 1)[0]
         self.size = self.mass / MASS * SIZE
 
-    def _update_particle_pos(self):
-        self.x = self.x + self.vx * FRAMERATE
-        self.y = self.y + self.vy * FRAMERATE
-
 class Particles:
+
     def __init__(self):
         self.particles = []
         for i in range(BODIES):
             self.particles.append(Particle())
 
     def _update_velocities(self):
-
         for i in range(BODIES):
-            acc_vec = [0,0]
+            self.particles[i].ax = 0
+            self.particles[i].ay = 0
             for j in range(BODIES):
                 if i == j:
                     continue
                 dx = self.particles[j].x-self.particles[i].x
                 dy = self.particles[j].y-self.particles[i].y
                 dist = math.sqrt(dy**2 + dx**2)
-                unit_vector = [divide(dx, dist), divide(dy, dist)]
-                acc = G_CONSTANT*divide((self.particles[i].mass * self.particles[j].mass),(dist**2))
-
+                unit_vector = [dx/dist, dy/dist]
+                acc = G_CONSTANT*divide((self.particles[i].mass * self.particles[j].mass),(dist**2), dist)
                 self.particles[i].ax += acc*unit_vector[0]
                 self.particles[i].ay += acc*unit_vector[1]
 
-            self.particles[i].vx -= self.particles[i].ax
-            self.particles[i].vy += self.particles[i].ay
+            self.particles[i].vx += self.particles[i].ax * FRAMERATE
+            self.particles[i].vy += self.particles[i].ay * FRAMERATE
 
-def divide(num, den):
+    def _update_particle_pos(self):
+        for i in range(BODIES):
+            self.particles[i].x += self.particles[i].vx
+            self.particles[i].y += self.particles[i].vy
+            array_of_circles[i].move(bodies.particles[i].vx, bodies.particles[i].vy)
+
+def divide(num, den, dis):
     if den == 0:
         return 0
     return num/den
@@ -64,6 +66,7 @@ if __name__ == "__main__":
     win.setBackground("gray")
     bodies = Particles()
     array_of_circles = []
+
     for i in range(BODIES):
         b = Circle(Point(bodies.particles[i].x,bodies.particles[i].y), bodies.particles[i].size)
         b.setOutline('blue')
@@ -72,9 +75,9 @@ if __name__ == "__main__":
         b.draw(win)
 
     while (True):
-        for i in range(BODIES):
-            array_of_circles[i].move(bodies.particles[i].vx, bodies.particles[i].vy)
-        time.sleep(TIMESTEP)
         bodies._update_velocities()
+        bodies._update_particle_pos()
+        time.sleep(TIMESTEP)
+
     win.getMouse()
     win.close()
